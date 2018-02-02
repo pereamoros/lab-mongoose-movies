866fc070bfb930010a8adbe524450756a57f5abc
Pere Amorós <amorospere@gmail.com>
2018-02-02 11:24:36 +0100

    Iteration 3 and 4 and 5

--- starter-code/celebrities-movies/routes/celebrities.js
+++ starter-code/celebrities-movies/routes/celebrities.js
@@ -5,7 +5,7 @@ const Celebrity = require('../models/celebrity');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  const Celebrity = require('../models/celebrity'); 
+   
   Celebrity.find({}, (err, result) => {
     if(err){
       next();
@@ -18,47 +18,54 @@ router.get('/', (req, res, next) => {
     }
   })
 });
+
 router.get('/:id', (req, res, next) => {
-  const Celebrity = require('../models/celebrity'); 
-  const celebrityId = req.8.id;
-  console.log(celebrityId);
-  Celebrity.findOne(celebrityId, (err, result) => {
+  const celebrityId = req.params.id;
+  Celebrity.findById(celebrityId, (err, result) => {
     if(err){
       next();
     }
     else{
       let celebrity = {
-        name,
-        occupation,
-        catchPhrase
+        name : result.name,
+        occupation : result.occupation ,
+        catchPhrase : result.catchPhrase
       }
-      res.redirect('celebrities/show', celebrity)
+      res.render('celebrities/show', celebrity)
     }
   })
 });
 
-// router.get('/new', (req, res, next) => {
-//   res.render('drones/new');
-// });
+router.get('/new', (req, res, next) => {
+  res.render('celebrities/new');
+});
 
-// router.post('/', (req, res, next) => {
-//   let newDrone = {
-//     name : req.body.name,
-//     propellers : req.body.propeller,
-//     maxSpeed : req.body.maxspeed
-//   }
-  
-//   const droneNew = new Drone(newDrone);
+router.post('/', (req, res, next) => {
+  let newCelebrity = {
+    name : req.body.name,
+    occupation : req.body.occupation,
+    catchPhrase : req.body.quote
+  }
+  const celebAdd = new Celebrity(newCelebrity);
+  celebAdd.save((err) => {
+    if (err) {
+      next();
+    } else {
+      res.redirect('celebrities');
+    }
+  });
+});
 
-//   droneNew.save((err) => {
-//     if (err) {
-//       next();
-//     } else {
-//       res.redirect('drones');
-//     }
-//   });
-
-
-// });
+router.post('/:id/delete', (req, res, next) => {
+  const celebrityId = req.params.id;
+  Celebrity.findByIdAndRemove(celebrityId, (err, result) => {
+    if(err){
+      next();
+    }
+    else{
+      res.redirect('/celebrities')
+    }
+  })
+});
 
 module.exports = router;
